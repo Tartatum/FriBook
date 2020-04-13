454e70160eca596360e25d861abe519d0cf784bd
Tartatum <45455096+Tartatum@users.noreply.github.com>
2020-04-13 11:04:05 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -35,44 +35,108 @@ Name: <input type="text" name="appendfile">
 </form>
 <h3>Filter</h3>
 <form action="index.php" method="post">
-<input type="text" name="nameFilter" value="">
+<input type="text" name="nameFilter" value=""> 
 <input type="checkbox" name="startingWith" value="TRUE">Only names starting with</input>
 <input type="submit" value="Filter list">
+<br>(Be careful it's case sensitive -> M =/=m)
 </form>
-<?php
-if( empty($_POST["startingWith"]) ) { echo "Not Checked"; }
-else { echo "Checked"; }
-?>
 
 
 <h1>My best friends:</h1>
 <ul>
-
+<form action="index.php" method="post">
 <?php
 
+
 $filename = 'friends.txt';
-if (isset($_POST['appendfile'])) {
+if (!empty($_POST['appendfile']) ) {
     $apptext = $_POST['appendfile'];
-    
     $file = fopen($filename, "a") or die("Unable to find the textfile");
-    fwrite($file, "\n" . $apptext);
+    fwrite($file, $apptext. "\n");
     fclose($file);
 }
 $nameFilter="";
+
 if (isset($_POST['nameFilter'])) {
     $nameFilter = $_POST['nameFilter'];
 }
+$filename = 'friends.txt';
+if(filesize($filename) !=0){
+    $file = fopen($filename, "r") or die("Unable to find the textfile");
+	$list = array();
+	while (!feof($file)){
+	$name = fgets($file);
+	if(!empty($name)){
+	array_push($list,$name);}
+	}
+fclose($file);
+	if (isset($_POST['delete'])) {
+        $indexToBeRemoved = $_POST['delete'];
+        unset($list[$indexToBeRemoved]);
+        $list = array_filter($list);
+		$file = fopen($filename, "w") or die("Unable to find the textfile");
+		foreach($list as $name){
+		if (!empty($name)){
+		fwrite($file,$name);
+		}
+		}
+		fclose($file);
+    }
+
+
+/*foreach ($list as $name) */
+$i=0;
+
+foreach ($list as $name){
+ if ($nameFilter=="" || strpos($name, $nameFilter)!==FALSE) {
+        if (empty($_POST["startingWith"])) {
+            echo "<li>$name ";
+            echo "<button type='submit' name='delete' value='$i'>Delete</button>";
+        } else {
+            if (strpos($name,$nameFilter)==0) {
+            echo "<li>$name ";
+            echo "<button type='submit' name='delete' value='$i'>Delete</button>";
+            }
+        }
+        
+        
+    }
+	$i++;
+	
+}
+}
+else
+{echo "File empty, change the txt's content or use the new friend.";}
+
+
+
+
+
+
+
+/*
 $file = fopen($filename, "r") or die("Unable to find the textfile");
 while (!feof($file)) {
     $name = fgets($file);
     if ($nameFilter=="" || strpos($name, $nameFilter)!==FALSE) {
-        echo "<li>$name</li>";
+        if (empty($_POST["startingWith"])) {
+            echo "<li>$name ";
+            echo "<button type='submit' name='delete' value='delete'>Delete</button></li>";
+        } else {
+            if (strpos($name,$nameFilter)==0) {
+            echo "<li>$name ";
+            echo "<button type='submit' name='delete' value='delete'>Delete</button></li>";
+            }
+        }
+        
+        
     }
     
 }
 fclose($file);
-
+*/
 ?>
+</form>
 </ul>
 
 
